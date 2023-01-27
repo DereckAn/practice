b2ca2f11160f11e95a8be2189526789b3a49aba8
Dereck <108163041+DereckAn@users.noreply.github.com>
2023-01-27 00:30:28 -0700

    Learning FastApi

---
+++ FastApi/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# uvicorn main:app --reload
+# Documentacion con Swagger: /docs
+# Documentacion con Redocly: /redocs
+# http://127.0.0.1:8000
+
+@app.get("/")
+async def root():
+    return "Hola"
+
+
+@app.get("/url")
+async def root():
+    return {"url_curso" : "https://mouredev.com/python"}
+
+
+
