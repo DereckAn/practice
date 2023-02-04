45238ced3052c7615de452981c1477bd6a143911
DereckAn <dereckangeles15071998@gmail.com>
2023-02-03 19:16:38 -0700

    practicas con flutter.

--- flutter/flutter_application_1/lib/main.dart
+++ flutter/flutter_application_1/lib/main.dart
@@ -33,9 +33,18 @@ class MyHomePage extends StatelessWidget {
         child: Icon(Icons.accessibility_new),
         onPressed: () {},
       ),
-      backgroundColor: Color.fromARGB(255, 69, 44, 126),
-      appBar: AppBar(title: Text("Material App")),
+      backgroundColor: Color.fromARGB(255, 44, 126, 60),
+      appBar: AppBar(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          title: Text("Material App"),
+          elevation: 0,
+          actions: <Widget>[
+            IconButton(onPressed: _add, icon: Icon(Icons.accessible_sharp))
+          ]),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Center(
             child: Text(
@@ -58,8 +67,62 @@ class MyHomePage extends StatelessWidget {
                 print("hola");
               },
               icon: Icon(Icons.favorite_border)),
+          Container(
+            color: Colors.pink.shade900,
+            height: 100,
+            width: 100,
+          ),
+          Row(
+            children: <Widget>[
+              Expanded(child: Text("Activar sonido")),
+              Switch(value: true, onChanged: (value) {}),
+            ],
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text("Activar camara"),
+              Switch(value: true, onChanged: (value) {}),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.amber.shade800,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Color.fromARGB(255, 24, 148, 167),
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Color.fromARGB(100, 115, 41, 172),
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Color.fromARGB(100, 84, 12, 31),
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Color.fromARGB(255, 225, 225, 29),
+                height: 50,
+                width: 50,
+              )
+            ],
+          )
         ],
       ),
     );
+  }
+
+  void _add() {
+    print("Hola Popo");
   }
 }
