898c82fcf2d9156e56f64e7cec38e28deaa748ab
DereckAn <dereckangeles15071998@gmail.com>
2023-02-09 07:29:28 -0700

    Musica, descargador de musica

---
+++ Python/practice/Downmusica.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+import os 
+url = "https://www.youtube.com/watch?v=7Z2hmz2NlYQ"
+video = YouTube(url)
+# print("title :" , video.title)
+out_path = video.streams.filter(only_audio=True).first().download("D:\Fotos\Videos descargados de youtube por python\Musica_trans")
+
+new_name = os.path.splitext(out_path) #[0] + ".mp3"
+os.rename(out_path, new_name[0] + ".mp3")
+print("done")
+
