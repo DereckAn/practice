ccbb72e79fa8ccddbb32f73964d8a72324d8281a
DereckAn <dereckangeles15071998@gmail.com>
2023-03-12 05:52:59 -0600

    practicas de NodeJs

---
+++ NodeJs/app.js
@@ -0,0 +1,14 @@
+//para correr el programa se debe de ejecutar el archivo app.js 
+// y para eso tienes que entrar a la terminar y poner "node app.js"
+//  "node {nombre de tu archivo}"
+
+console.log("Hello World");
+
+const amount = 12;
+if (amount<10){
+    console.log("Less than 10");
+} else {
+    console.log("Greater than 10");
+}
+
+console.log("It is my first node app");
