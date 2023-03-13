08071170e3a72743e4032d5a61d9045b4b0eea66
DereckAn <dereckangeles15071998@gmail.com>
2023-03-13 16:01:31 -0600

    Differente ways of Modules

--- NodeJs/4-names.js
+++ NodeJs/4-names.js
@@ -5,8 +5,10 @@ const secret = "Super secret"
 const pedro = "pedro";
 const juan = "juan";
 
-module.exports ={  //esta es un amanera de exportar las variables.
+// console.log(module); Eso es para ver toda la informacion de este archivo
+
+
+module.exports ={  //esta es un amannera de exportar las variables.
     pedro,
     juan
 } // estamos poniendo los valores en unas llaves porque son varios valores. 
-console.log(module);
---
+++ NodeJs/6-alternative.js
@@ -0,0 +1,7 @@
+module.exports.items = ["item", "item2", "item3"]; // esta es otra forma de exportar
+const person = {
+    name: "Dereck",
+
+}
+
+module.exports.persona = person;
