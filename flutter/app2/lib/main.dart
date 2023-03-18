7bd93860f66b27e015c6c933e1505feedb800b87
DereckAn <dereckangeles15071998@gmail.com>
2023-03-17 23:01:21 -0600

    Eliminar transaction list y fucionnarlo con main Leccion 95 - Flutter

--- flutter/app2/lib/main.dart
+++ flutter/app2/lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:app2/widgets/user_transactions.dart';
+import 'package:app2/widgets/new_transaction.dart';
+// import 'package:app2/widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
 
-
+import 'models/transaction.dart';
+import 'widgets/transaction_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,17 +24,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
   // late String titleInput;  Estas variables son de una manera de caputar el input.
-  // late String amounInput;
+
+
+
+   final List<Transaction> _transactions = [
+    Transaction(id: '01', title: "Water", amount: 10.00, date: DateTime.now()),
+    Transaction(id: '02', title: "Coca", amount: 15.36, date: DateTime.now()),
+  ];
+
+  void _addNewtransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+    setState(() {
+      _transactions.add(newTx);
+    });
+  }
+  
+  void startAddNewTransaction(BuildContext ctx) {
+    // Este metodo hace que al precionar el boton podamos agregar la informacion para el nuevo elemento
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return  GestureDetector( onTap: () {},behavior: HitTestBehavior.opaque, child: NewTransaction(_addNewtransaction), );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Flutter app"),
-        actions: <Widget>[IconButton(onPressed: (){}, icon: const Icon(Icons.add))],
+        actions: <Widget>[
+          IconButton(onPressed: ()=> startAddNewTransaction(context), icon: const Icon(Icons.add))
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -48,7 +82,7 @@ class MyHomePage extends StatelessWidget {
                   child: const Text(
                       "Chart")), // Eleevation es para de tenga el efecto visial como de que un boton esta sobresaliendo.
             ),
-            const UserTransaction(),
+            ListTrans(_transactions),
             const Card(
               color: Colors.red,
               child: Text("List of Tx"),
@@ -56,8 +90,10 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(onPressed: (){}, child: const Icon(Icons.menu)),
+      floatingActionButtonLocation: FloatingActionButtonLocation
+          .centerFloat, // Esto es para controlar la ubicacion del boton flotante.
+      floatingActionButton:
+          FloatingActionButton(onPressed:  ()=> startAddNewTransaction(context), child: const Icon(Icons.add)),
     );
   }
 }
--- flutter/app2/lib/widgets/user_transactions.dart
+++ flutter/app2/lib/widgets/user_transactions.dart
@@ -1,40 +1,26 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import '../models/transaction.dart';
-import './new_transaction.dart';
-import './transaction_list.dart';
+// import '../models/transaction.dart';
+// import './new_transaction.dart';
+// import './transaction_list.dart';
 
-class UserTransaction extends StatefulWidget {
-  const UserTransaction({super.key});
+// class UserTransaction extends StatefulWidget {
+//   const UserTransaction({super.key});
 
-  @override
-  State<UserTransaction> createState() => _UserTransactionState();
-}
+//   @override
+//   State<UserTransaction> createState() => _UserTransactionState();
+// }
 
-class _UserTransactionState extends State<UserTransaction> {
-  final List<Transaction> _transactions = [
-    Transaction(id: '01', title: "Water", amount: 10.00, date: DateTime.now()),
-    Transaction(id: '02', title: "Coca", amount: 15.36, date: DateTime.now()),
-  ];
+// class _UserTransactionState extends State<UserTransaction> {
+ 
 
-  void _addNewtransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-        title: txTitle,
-        amount: txAmount,
-        date: DateTime.now(),
-        id: DateTime.now().toString());
-    setState(() {
-      _transactions.add(newTx);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        NewTransaction(_addNewtransaction),
-        ListTrans(_transactions),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewtransaction),
+//         ListTrans(_transactions),
+//       ],
+//     );
+//   }
+// }
