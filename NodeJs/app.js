fbda7bd2ce76f49de1266b6e85374715cf1fc4b1
DereckAn <dereckangeles15071998@gmail.com>
2023-03-22 02:36:45 -0600

    nodeJs sync and async

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -31,26 +31,3 @@
 
 // Modules - Encapsulated Code (only share minimum)
 // CommonJs - every files is module (by default)
-
-
-const os = require("os");
-
-//info about current user
-const user = os.userInfo();
-
-console.log(user)
-
-
-//method returns the system uptime in seconds
-console.log(`The ystem uptime is  ${os.uptime()} seconds`);
-
-
-
-
-
-const currentOs = {
-    name: os.type,
-    release: os.release,
-    totalMem: os.totalmem,
-    freeMem: os.freemem,
-}
