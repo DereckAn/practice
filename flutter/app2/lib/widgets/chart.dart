3285ecffc1b7aebff831d90cc55368eb358bdd59
DereckAn <dereckangeles15071998@gmail.com>
2023-03-31 03:19:57 -0600

    maidificando el char y empezandolo a hacer responsive

--- flutter/app2/lib/widgets/chart.dart
+++ flutter/app2/lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:app2/models/transaction.dart';
+import 'package:app2/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -11,21 +12,35 @@ class Chart extends StatelessWidget {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
       double totalSum = 0.0;
-      for (var i = 0; i < recentTransactions.length; i++) {
-        // hay ptra manera de poner el "for"  for( var tx in recentTransactions )
-        if (recentTransactions[i].date.day == weekDay.day &&
-            recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.year == weekDay.year) {
-          totalSum += recentTransactions[i].amount;
+      // for (var i = 0; i < recentTransactions.length; i++) {
+      //   // hay ptra manera de poner el "for"  for( var tx in recentTransactions )
+      //   if (recentTransactions[i].date.day == weekDay.day &&
+      //       recentTransactions[i].date.month == weekDay.month &&
+      //       recentTransactions[i].date.year == weekDay.year) {
+      //     totalSum += recentTransactions[i].amount;
+      //   }
+      // }
+      for (var tx in recentTransactions) {
+        if (tx.date.day == weekDay.day &&
+            tx.date.month == weekDay.month &&
+            tx.date.year == weekDay.year) {
+          totalSum += tx.amount;
         }
       }
 
       // print(DateFormat.E().format(weekDay));
       // print(totalSum);
       return {
-        'day': DateFormat.E().format(weekDay).substring(0, 2),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum
       };
+    });
+  }
+
+  double get totalSpending {
+    return groundTransactionValues.fold(0.0, (sum, item) {
+      // investigar la funcion fold
+      return sum + (item['amount'] as double);
     });
   }
 
@@ -35,10 +50,23 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
-      child: Row(
-          children: groundTransactionValues.map((data) {
-        return Text('${data['day']}: ${data['amount']}');
-      }).toList()),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groundTransactionValues.map((data) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: CharBar(
+                    data['day'] as String,
+                    data['amount']
+                        as double, //  que no se te olbide cast los datos
+                    totalSpending == 0.0
+                        ? 0.0
+                        : (data["amount"] as double) / totalSpending),
+              );
+            }).toList()),
+      ),
     );
   }
 }
--- flutter/app2/lib/widgets/chart_bar.dart
+++ flutter/app2/lib/widgets/chart_bar.dart
@@ -12,7 +12,9 @@ class CharBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text("\$${spendingAmount.toStringAsFixed(0)}"),
+        FittedBox( // este widget forza a su hijo adentro del espacio disponible
+            child: Text(
+                "\$${spendingAmount.toStringAsFixed(1)}")), // esta parte es para redondear
         const SizedBox(height: 4),
         Container(
           height: 60,
@@ -22,7 +24,7 @@ class CharBar extends StatelessWidget {
               Container(
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey, width: 1.0),
-                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  color: const Color.fromARGB(124, 135, 127, 127),
                   borderRadius: BorderRadius.circular(10),
                 ),
               ),
