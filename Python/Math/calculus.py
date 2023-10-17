77ba0590369acfef6cc0a6436be8164e8eb30339
Dereck <dereckangeles15071998@gmail.com>
2023-10-16 19:41:51 -0600

    cosas dd practica

--- Python/Math/calculus.py
+++ Python/Math/calculus.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from IPython.display import display, Math
 
-
+ 
 '''---------------------------------------------------------------------------------------------------------------------118'''
 # Computing limist of a function. 
 
